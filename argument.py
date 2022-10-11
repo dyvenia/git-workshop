379f17e6e607448e3b29b6404d7559454995a3b0
IrynaKovalchyk <irenkovalchik@gmail.com>
2022-10-11 16:38:13 +0200

    Modified file

--- argument.py
+++ argument.py
@@ -5,7 +5,7 @@ def main(name: str =  typer.Argument(None)):
     if name is None:
         print("Hello World")
     else:
-        print(f"Hello {name}")
+        print(f"Hello {name}!!!")
 
 
 if __name__ == "__main__":
