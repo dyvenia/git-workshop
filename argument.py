87c520069fff258def5828b065e3adbf141c6912
IrynaKovalchyk <irenkovalchik@gmail.com>
2022-10-11 13:40:56 +0200

    Adding typer for CLI

---
+++ argument.py
@@ -0,0 +1,12 @@
+import typer
+
+
+def main(name: str =  typer.Argument(None)):
+    if name is None:
+        print("Hello World")
+    else:
+        print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
