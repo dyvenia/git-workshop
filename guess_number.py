e7d4f1f7f6c4a2f1e175af95d388c8848333a748
djagoda881 <djagoda881@gmail.com>
2023-05-09 16:56:27 +0200

    ♻️ Refactored code in guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,11 @@
 import random
 
 number = random.randint(0, 101)
-user_number = input("Try to guess the number rom 0 to 100: ")
-while int(user_number) != number:
-    if int(user_number) > number:
-        user_number = input("Your number is too big. Try once again: ")
+user_number = int(input("Try to guess the number rom 0 to 100: "))
+while user_number != number:
+    if user_number > number:
+        user_number = int(input("Your number is too big. Try once again: "))
     else:
-        user_number = input("Your number is too small. Try once again: ")
+        user_number = int(input("Your number is too small. Try once again: "))
 else:
     print("Congratulations, you won!")
