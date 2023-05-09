ad0ba777489fdbf7e56270c9fc2d0f8afb496eec
djagoda881 <djagoda881@gmail.com>
2023-05-09 16:51:23 +0200

    ♻️  Changed variable name in guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,11 @@
 import random
+
 number = random.randint(0, 101)
-my_number= input("Try to guess the number rom 0 to 100:")
-while int(my_number) != number:
-    if int(my_number) > number:
-        my_number = input("Your number is too big. Try once again:")
+user_number = input("Try to guess the number rom 0 to 100:")
+while int(user_number) != number:
+    if int(user_number) > number:
+        user_number = input("Your number is too big. Try once again:")
     else:
-        my_number = input("Your number is too small. Try once again:")
+        user_number = input("Your number is too small. Try once again:")
 else:
     print("Congratulations, you won!")
-   
-   
