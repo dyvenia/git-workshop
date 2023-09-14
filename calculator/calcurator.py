c126b4cd7f615d2f713ef1414ab59d9d6f8a8b56
djagoda881 <djagoda881@gmail.com>
2023-09-14 14:43:20 +0200

    🎉 Started calculator project

---
+++ calculator/calcurator.py
@@ -0,0 +1,18 @@
+def add(x, y):
+    """Returns the sum of x and y."""
+    return x + y
+
+def multiply(x, y):
+    """Returns the product of x and y."""
+    return x * y
+
+def divide(x, y):
+    """Returns the result of dividing x by y."""
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Division by zero"
+
+def subtract(x, y):
+    """Returns the difference between x and y."""
+    return x - y
