ef68bcb8d1a883ac94e11971b319eed6a0aea448
acabrerarod <acabrera@dyvenia.com>
2024-02-09 12:39:45 +0000

    added exponentiating and corrected file name

---
+++ calculator/calculator.py
@@ -0,0 +1,34 @@
+# calculator/calculator.py
+
+
+def add(x, y):
+    """Returns the sum of x and y."""
+    return x + y
+
+
+def multiply(x, y):
+    """Returns the product of x and y."""
+    return x * y
+
+
+def divide(x, y):
+    """Returns the result of dividing x by y."""
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Division by zero"
+
+
+def subtract(x, y):
+    """Returns the difference between x and y."""
+    return x - y
+
+
+def exponentiate(x, y):
+    """Returns x raised to the power of y."""
+    return x**y
+
+
+def square_root(x):
+    """Returns the square root of x."""
+    return x ** (1 / 2)
